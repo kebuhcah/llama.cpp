bda18bf1e77a93529b50666d44477b84d13f9f2c
Kevin Sun <kevsun1@gmail.com>
2023-03-26 17:57:38 -0700

    Modifications to run new version of Alpaca model

--- examples/alpaca.sh
+++ examples/alpaca.sh
@@ -7,4 +7,4 @@
 cd `dirname $0`
 cd ..
 
-./main -m ./models/ggml-alpaca-7b-q4.bin --color -f ./prompts/alpaca.txt -ins -b 256 --top_k 10000 --temp 0.2 --repeat_penalty 1 -t 7
+./main -m ./models/alpaca/13B/ggml-model-q4_0.bin --color -f ./prompts/alpaca.txt -ins --n_parts 1 -b 256 --top_k 10000 --temp 0.2 --repeat_penalty 1 -t 7
